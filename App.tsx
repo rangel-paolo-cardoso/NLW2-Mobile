11613ed1fd3982b072466e22423b134988216840
Rangel Paolo <paolorangel1996@outlook.com>
2020-08-06 22:46:34 -0300

    Criada rota para LandingPage.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import Landing from './src/pages/Landing';
+
 import { AppLoading } from 'expo';
 import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
 import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
 
-export default function App() {
+import AppStack from './src/routes/AppStack';
+
+const App = () => {
   let [fontsLoaded] = useFonts({
     Archivo_400Regular,
     Archivo_700Bold,
@@ -18,9 +20,11 @@ export default function App() {
   } else {
     return (
       <>
-        <Landing />
+        <AppStack />
         <StatusBar style="light" />
       </>
     );
   }
-}
+};
+
+export default App;
